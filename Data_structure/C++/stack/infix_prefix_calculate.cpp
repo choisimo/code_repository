6f9d86ceb7fed35815e570bab5e4c35bde73c91c
choisimo <csimon2k0209@gmail.com>
2024-03-21 18:22:20 +0900

    diner time

--- Data_structure/C++/stack/infix_prefix_calculate.cpp
+++ Data_structure/C++/stack/infix_prefix_calculate.cpp
@@ -40,9 +40,26 @@ public:
 
     int pop(){
         if (isEmpty()) {
-            throw out_of_range("stack overflow");
+            throw out_of_range("stack underflow");
         }
-        // 데이터 제거 후 top 변수 감소 
+        // 데이터 제거 후 top 변수 감소
         return array[top--];
     }
+
+    int peek(){
+        if (isEmpty()) {
+            throw out_of_range("stack underflow");
+        }
+        return array[top];
+    }
+
+    int size() const{
+        return top + 1;
+    }
 };
+
+int main(){
+    myStack stack1(100);
+    
+    return 0;
+}
