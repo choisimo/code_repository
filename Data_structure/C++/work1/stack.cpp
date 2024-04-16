ad17dba261d6976b819dc2ecfa42607bcd6a4ac5
choisimo <csimon2k0209@gmail.com>
2024-04-16 14:00:35 +0900

    fs

--- Data_structure/C++/work1/stack.cpp
+++ Data_structure/C++/work1/stack.cpp
@@ -1,14 +1,25 @@
-//
-// Created by csimo on 3/21/2024.
-//
 #include <iostream>
-#include <stack>
+#include <string>
 
 using namespace std;
 
-int main(){
-    int n1 = 5; int n2 = 4;
-    if (n1 > 1 || n2 < 2){
-        cout << "1" << endl;
-    }
+struct what {
+  int id;
+  string name;
+  what(int id, const string* name){
+    cout << "name 의 값은 : " << *name << endl;
+    this->id =  id;
+    this->name = *name;
+  }
+};
+
+int main() {
+    string str = "123";
+    string *str_mem = &str;
+    cout << "str_mem 주소 값 : " << str_mem << endl;
+    cout << "str_mem 포인터 변수의 주소 : " 
+    << &str_mem << endl;
+    what myWhat(1, &str); // 수정된 코드
+    what myWhat2(myWhat);
+    return 1;
 }
