bc4bcbbebe565dcf109f553a2e241980e9a563f4
choisimo <150008602+choisimo@users.noreply.github.com>
2024-04-17 03:03:33 +0900

    reposit

--- Data_structure/C++/homework2/inheritage_Stack_queue.cpp
+++ Data_structure/C++/homework2/inheritage_Stack_queue.cpp
@@ -4,9 +4,10 @@
 
 
 #include <stdexcept>
+#include <iostream>
 
 class DataStructure {
-protected:
+private:
     int* array;
     int capacity;
     int ptr;
@@ -27,6 +28,28 @@ protected:
         array = newArray;
         capacity = newCapacity;
     }
+    void setPtr(int ptr){
+        this->ptr = ptr;
+    };
+    void setArray(int index, int item){
+        if (index >= 0 && index < capacity){
+            array[index] = item;
+        }
+    }
+    int getPtr() const {
+        return ptr;
+    }
+    int getCapacity() const{
+        return capacity;
+    }
+    int getArray(int index) const {
+        if (index >= 0 && index < capacity){
+            return array[index];
+        } else {
+            throw std::out_of_range("out of range");
+        }
+    }
+
 public:
     explicit DataStructure(int capacity) : capacity(capacity) {  array = new int[capacity];
     }
@@ -48,8 +71,84 @@ public:
 };
 
 class Stack : public DataStructure{
-    public Stack
-    bool isEmpty() const override {
-
+public:
+    explicit Stack(int capacity) : DataStructure(capacity){
+        this->setPtr(-1);
     }
+    bool isEmpty() const override {
+        return this->getPtr() == -1;
+    }
+    bool isFull() const override{
+        return this->getPtr() == getCapacity() -1;
+    }
+    void add (int item) override{
+        if (isFull()){
+            this->increaseCapacity();
+        }
+        setPtr(getPtr() + 1);
+        setArray(getPtr(), item);
+    }
+    int remove() override{
+        if(isEmpty()){
+            throw std::runtime_error("stack underflow");
+        } else if (getPtr() <= getCapacity()/2){
+            this->decreaseCapacity();
+        }
+        int item = getArray(getPtr());
+        setPtr(getPtr()-1);
+        return item;
+    }
+    int size() override{
+        return this->getPtr() + 1;
+    }
+    void printElements() const override{
+        for (int i = 0; i <= getPtr(); i++){
+            std::cout << getArray(i) << ", ";
+        }
+        std::cout << std::endl;
+    };
+    ~Stack(){};
+};
+
+class Queue: public DataStructure{
+private:
+    int front;
+    int rear;
+public:
+    explicit Queue(int capacity) : DataStructure(capacity), front(0), rear(-1){}
+
+    bool isEmpty() const override{
+        return (front == rear + 1);
+    }
+    bool isFull() const override{
+        return (rear == this->getCapacity() - 1);
+    }
+    void add(int item) override{
+        if (isFull()){
+            this->increaseCapacity();
+        }
+        setArray(++rear, item);
+    }
+    int remove() override{
+        int total_size = rear - front;
+        if (isEmpty()){
+            throw std::runtime_error("Queue is Empty");
+        } else if ((total_size) <= getCapacity() / 2){
+            this->decreaseCapacity();
+            front = 0;
+            rear = total_size;
+        }
+        int item = getArray(front++);
+        return item;
+    }
+    int size() override{
+        return rear - front + 1;
+    }
+    void printElements() const override{
+        for (int i = front; i <= rear; i++){
+            std::cout << getArray(i) << ", ";
+        }
+        std::cout << std::endl;
+    };
+    ~Queue(){};
 };
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/workspace/github/code_repository
+# Install script for directory: C:/WorkSpace/git/code_repository
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/workspace/github/code_repository/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/WorkSpace/git/code_repository/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
